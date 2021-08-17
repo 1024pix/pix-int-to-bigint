5b5750e444ed7bb6f07c82cc550e7ff7494635c5
Pierre TOP (TOPI) <ptop@octo.com>
2021-08-17 16:22:05 +0200

    Add activity faking

---
+++ unreferenced-pk/fake-activity.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const bunyan = require('bunyan');
+const logger = bunyan.createLogger({name: 'pix_bigint'});
+const {Client} = require('pg');
+
+const agent = async (query, interval) => {
+   const connectionString = process.env.DATABASE_URL;
+   const client = new Client(connectionString);
+   await client.connect();
+
+   while (true) {
+      try {
+         await client.query(query);
+      } catch (error) {
+         logger.error(error);
+      }
+      await new Promise((resolve) => {
+         setTimeout(resolve, interval);
+      });
+
+   }
+
+}
+
+const launchAgents = async () => {
+
+   const queries = JSON.parse(process.env.QUERIES);
+
+   const agents = queries.flatMap((query) => {
+      let promises = [];
+      for (let i = 0; i < query.agentCount; i++) {
+         promises.push(agent(query.text, query.interval));
+      }
+      return promises
+   });
+
+   await Promise.all(agents);
+}
+
+(async () => {
+   try {
+      await launchAgents();
+   } catch (error) {
+      logger.error(error);
+   }
+})()
